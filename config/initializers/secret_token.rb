fabb7ad579a0e60d40d4746607f893d25944ad1b
Jonathan Lai <jonathanlai@eltux.net>
2013-04-21 13:17:09 -0700

    4/21/13 1315pm save

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GaMapQuery::Application.config.secret_token = '51bcd3f3749c8257a5c962fc4ca2b4fabb95fcc334f8944e69dea725c8678ee0ea3e4baa086ff50f60ed7dfd9626ed288255de0495d61424ac4e3fbf4a2b45d8'
